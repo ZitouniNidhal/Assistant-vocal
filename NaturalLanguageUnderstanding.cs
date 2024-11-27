7967a810288549a4563ee0ad9cd02a613e88429b
ZITOUNI NIDHAL <nidhalzitouni111@gmail.com>
2024-11-27 13:43:34 +0100

    add function analyse (NaturalLanguageUnderstanding)

--- NaturalLanguageUnderstanding.cs
+++ NaturalLanguageUnderstanding.cs
@@ -1,11 +1,35 @@
+using System;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime.Models;
+using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
+
 namespace VoiceAssistant
 {
     public static class NaturalLanguageUnderstanding
     {
-        public static string AnalyzeCommand(string command)
+        private static readonly LuisRuntimeClient _luisClient;
+
+        static NaturalLanguageUnderstanding()
         {
-            // Utiliser des bibliothèques de traitement du langage naturel (comme LUIS, spaCy) pour analyser la commande.
-            return "Analyse de la commande en cours...";
+            _luisClient = new LuisRuntimeClient(new ApiKeyServiceClientCredentials("YOUR_LUIS_API_KEY"))
+            {
+                Endpoint = "https://YOUR_LUIS_REGION.api.cognitive.microsoft.com/"
+            };
+        }
+
+        public static (string Intent, Dictionary<string, string>) AnalyzeCommand(string command)
+        {
+            var request = new PredictionRequest { Query = command };
+            var response = _luisClient.Prediction.GetSlotPredictionAsync("YOUR_LUIS_APP_ID", "YOUR_LUIS_SLOT_NAME", request).Result;
+
+            var intent = response.TopIntent?.IntentName;
+            var entities = new Dictionary<string, string>();
+
+            foreach (var entity in response.Entities)
+            {
+                entities.Add(entity.Type, entity.Entity);
+            }
+
+            return (intent, entities);
         }
     }
 }
